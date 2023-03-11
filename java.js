de9131aea14ce3fe11c014c4bf07139de256c8d9
Conrado Simon <conradosimon2013@gmail.com>
2023-03-11 12:28:57 -0300

    bala

--- java.js
+++ java.js
@@ -7,10 +7,11 @@ let pj1left  = 0;
 let pj1right = 0;
 let pj1up = 0;
 let pj1down = 0;
-
+let fuego = 0;
 
 setInterval(code, 8);
 
+setInterval(fire, 8);
 
 
 function code(){
@@ -21,6 +22,10 @@ function code(){
 		document.getElementById('pj1').style.transform = 'scaleX(1)';
 		document.getElementById('pj1').style.backgroundImage = 'url(derecha.png)';
 		
+		if(fuego == 0){
+		document.getElementById('bala').style.left = `${positionpj1}px`;
+		document.getElementById('bala').style.bottom = `${positionypj1}px`;
+			}
 			
 
 	}
@@ -34,6 +39,10 @@ function code(){
 		document.getElementById('pj1').style.transform = 'scaleX(-1)';
 		document.getElementById('pj1').style.backgroundImage = 'url(derecha.png)';
 		
+		if(fuego == 0){
+		document.getElementById('bala').style.left = `${positionpj1}px`;
+		document.getElementById('bala').style.bottom = `${positionypj1}px`;
+			}
 			}
 
 
@@ -47,6 +56,10 @@ function code(){
 		document.getElementById('pj1').style.backgroundImage = 'url(derecha.png)';
 		
 			
+		if(fuego == 0){
+		document.getElementById('bala').style.left = `${positionpj1}px`;
+		document.getElementById('bala').style.bottom = `${positionypj1}px`;
+			}
 
 	}
 
@@ -55,8 +68,11 @@ function code(){
 		positionypj1-=1.5;
 		document.getElementById('pj1').style.bottom = `${positionypj1}px`;
 		document.getElementById('pj1').style.backgroundImage = 'url(derecha.png)';
-		
-			
+	
+		if(fuego == 0){
+		document.getElementById('bala').style.left = `${positionpj1}px`;
+		document.getElementById('bala').style.bottom = `${positionypj1}px`;
+			}
 
 	}
 
@@ -67,6 +83,8 @@ function code(){
 	if (pj1left==0 && pj1right==0) {
 		document.getElementById('pj1').style.backgroundImage = 'url(https://i.pinimg.com/originals/52/17/7e/52177e3843b7878a30f059b14d065f7d.png)';
 	
+		document.getElementById('bala').style.left = `${positionpj1}px`;
+		document.getElementById('bala').style.bottom = `${positionypj1}px`;
 	}
 
 	if (pj1up == 0) {
@@ -78,7 +96,24 @@ function code(){
 
 
 
+
+
 };
+
+
+	function fire() {
+		 if(!fuego==1) {setTimeout(function(){fuego=1}, 100);
+			document.getElementById('bala').style.left = `${positionpj1}px`;
+		  setTimeout(function(){document.getElementById('bala').style.bottom = `${positionypj1 + 20}px`;}, 170);
+		  setTimeout(function(){document.getElementById('bala').style.bottom = `${positionypj1 + 40}px`;}, 250);
+		  setTimeout(function(){document.getElementById('bala').style.bottom = `${positionypj1 + 80}px`;}, 320);
+		  setTimeout(function(){document.getElementById('bala').style.bottom = `${positionypj1 + 100}px`;}, 390);
+		  setTimeout(function(){document.getElementById('bala').style.bottom = `${positionypj1 + 120}px`;}, 440);
+		  setTimeout(function(){fuego=0}, 500);}
+	}
+
+
+
 
 
 document.addEventListener("keydown", function(event) {
